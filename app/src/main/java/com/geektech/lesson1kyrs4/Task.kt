8aa0f78b638c04196d7f67f5791595600b6f831f
jkbarganalieva <jkbarganalieva@gmail.com>
2023-01-17 16:29:10 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/lesson1kyrs4/Task.kt
@@ -0,0 +1,7 @@
+package com.geektech.lesson1kyrs4
+
+data class Task(
+    var title:String?=null,
+    var desc:String?=null
+):java.io.Serializable
+
---
+++ app/src/main/java/com/geektech/lesson1kyrs4/ui/profile/ProfileFragment.kt
@@ -0,0 +1,35 @@
+package com.geektech.lesson1kyrs4.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.geektech.lesson1kyrs4.R
+import com.geektech.lesson1kyrs4.databinding.FragmentNotificationsBinding
+import com.geektech.lesson1kyrs4.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
